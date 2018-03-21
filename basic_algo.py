Calculations are made with int only (until final calculation of pi) Pi is calculated only at the last iteration. Differential refining. Fixed filter_uniq_params (now it updates the filtered_params member, which is being used by filtered_params)

basic_algo.py
@@ -1,7 +1,8 @@
 from decimal import Decimal as dec
 import decimal
 from gen_real_pi import gen_real_pi
-import numpy as np
+import math
+# import numpy as np
 
 
 def set_precision(prec):
@@ -12,16 +13,17 @@ class RNNAlgo:
     class SufficientAccuracy(Exception):
         pass
 
-    def __init__(self, params, diff_mat_gen, first_diff_mat, target_val=None, dtype=dec):
+    def __init__(self, params, diff_mat_gen, first_diff_mat, target_val=None, logging=False, dtype=dec):
         self._params = params
         self._diff_mat_gen = diff_mat_gen
         self._first_diff_mat = first_diff_mat
         self._target_val = target_val
+        self._logging = logging
         self._dtype = dtype
 
-        self.gradient_vectors = []
         self.diff_mats = []
         self.params_log = {p:[] for p in self._params}
+        self.num_of_executed_iters = 0
 
     def reinitialize(self, params=None, first_diff_mat=None, **kwargs):
         if params is None:
@@ -32,25 +34,40 @@ class RNNAlgo:
         if first_diff_mat is not None:
             self._first_diff_mat = first_diff_mat
 
-    def gen_iterations(self, num_of_iters, iteration_algorithm):
+    def gen_iterations(self, num_of_iters, accuracy, iteration_algorithm):
         self.params_log = {p : [self._params[p]] for p in self._params}
-        self.gradient_vectors = []
         self.diff_mats = []
 
         params = self._params
         diff_mat = self._first_diff_mat
-        for i in range(num_of_iters):
-            try:
-                params, diff_mat, gradient_vec = iteration_algorithm(params, diff_mat, i)
-                for p in params:
-                    self.params_log[p].append(params[p])
-                self.diff_mats.append(diff_mat)
-                self.gradient_vectors.append(gradient_vec)
-            except self.SufficientAccuracy:
-                break
+        num_of_iters_div_100 = math.ceil(num_of_iters/100)
+        num_of_iters = num_of_iters_div_100 * 100
+        for i in range(num_of_iters_div_100):
+            old_params = params
+            old_diff_mat = diff_mat
+            for j in range(100):
+                try:
+                    params, diff_mat = iteration_algorithm(params, diff_mat, i*100+j)
+                    for p in params:
+                        if self._logging:
+                            self.params_log[p].append(params[p])
+                    if self._logging:
+                        self.diff_mats.append(diff_mat)
+                except self.SufficientAccuracy:
+                    break
+            # if self.is_accuracy_met(accuracy, old_params, old_diff_mat, i):
+            #     break
+        if hasattr(self, 'finalize_iterations'):
+            params, diff_mat = self.finalize_iterations(params, diff_mat, num_of_iters)
 
-    def get_derivative(self):
-        return np.multiply(self.compare_result(), self.gradient_vectors[-1])
+        if not self._logging:
+            for p in params:
+                self.params_log[p] = [params[p]]
+            self.diff_mats = [diff_mat]
+        self.num_of_executed_iters = i*100+j
+
+    def get_num_of_executed_iters(self):
+        return self.num_of_executed_iters
 
     def compare_result(self, parameter, target_val=None):
         if target_val is not None:
@@ -67,13 +84,13 @@ class RNNAlgo:
 
 
 class PiBasicAlgo(RNNAlgo):
-    def __init__(self, params, diff_mat_gen=None, first_diff_mat=None, target_val=None, dtype=dec):
+    def __init__(self, params, diff_mat_gen=None, first_diff_mat=None, target_val=None, logging=False, dtype=dec):
         if target_val is None:
             target_val = gen_real_pi()
-        super().__init__(params, diff_mat_gen, first_diff_mat, target_val, dtype)
+        super().__init__(params, diff_mat_gen, first_diff_mat, target_val, logging, dtype)
 
-    def gen_iterations(self, num_of_iters):
-        super().gen_iterations(num_of_iters, self.iteration_algorithm)
+    def gen_iterations(self, num_of_iters, accuracy):
+        super().gen_iterations(num_of_iters, accuracy, self.iteration_algorithm)
 
     def compare_result(self, real_pi=None):
         return super().compare_result('pi', real_pi)

cont_fracs.py
@@ -3,6 +3,7 @@ import basic_algo
 from decimal import Decimal as dec
 import decimal
 from gen_real_pi import gen_real_pi
+import math
 import time
 
 # used to avoid numpy casting int into numpy.int32/64, which doesn't handle bignums
@@ -15,32 +16,26 @@ class ZeroB(Exception):
 
 
 class PiContFrac(basic_algo.PiBasicAlgo):
-    def __init__(self, a_coeffs, b_coeffs, avoid_zero_b=True, check_b_threshold=False, b_threshold=10**10, diff_mat_gen=None,
-                 first_diff_mat=None, with_grad=False, target_val=None, dtype=dec):
-        if diff_mat_gen is None and with_grad:
-            diff_mat_gen = self._default_diff_mat_with_grad_gen
-        elif diff_mat_gen is None and not with_grad:
-            diff_mat_gen = self._default_diff_mat_no_grad_gen
+    def __init__(self, a_coeffs, b_coeffs, avoid_zero_b=False, check_b_threshold=False, b_threshold=10**10, diff_mat_gen=None,
+                 first_diff_mat=None, target_val=None, logging=False, dtype=int):
+        if diff_mat_gen is None:
+            diff_mat_gen = self._default_diff_mat_gen
         if target_val is None:
             target_val = gen_real_pi()
 
         self._dtype_0 = dtype(0)
         self._dtype_1 = dtype(1)
-        self._with_grad = with_grad
         params = {'a_coeffs': a_coeffs, 'b_coeffs': b_coeffs, 'pi': 1}
         if first_diff_mat is None:
             self._auto_first_diff_mat = True
-            if self._with_grad:
-                first_diff_mat = self._autogen_first_diff_mat_with_grad(params, dtype)
-            else:
-                first_diff_mat = self._autogen_first_diff_mat_no_grad(params, dtype)
+            first_diff_mat = self._autogen_first_diff_mat(params, dtype)
         else:
             self._auto_first_diff_mat = False
 
         self._avoid_zero_b = avoid_zero_b
         self._check_b_threshold = check_b_threshold
         self._b_threshold = dtype(b_threshold)
-        super().__init__(params, diff_mat_gen, first_diff_mat, target_val, dtype)
+        super().__init__(params, diff_mat_gen, first_diff_mat, target_val, logging, dtype)
 
     def reinitialize(self, a_coeffs=None, b_coeffs=None, first_diff_mat=None, **kwargs):
         params = kwargs
@@ -53,10 +48,7 @@ class PiContFrac(basic_algo.PiBasicAlgo):
         else:
             params['b_coeffs'] = self._params['b_coeffs']
         if first_diff_mat is None and self._auto_first_diff_mat:
-            if self._with_grad:
-                first_diff_mat = self._autogen_first_diff_mat_with_grad(params)
-            else:
-                first_diff_mat = self._autogen_first_diff_mat_no_grad(params)
+            first_diff_mat = self._autogen_first_diff_mat(params)
 
         super().reinitialize(params, first_diff_mat)
 
@@ -75,6 +67,8 @@ class PiContFrac(basic_algo.PiBasicAlgo):
 
         # new_vec_p = [ new_mat * diff_vec for new_mat, diff_vec in zip(diff_mats_p, diff_vec_p) ]
         # new_vec_q = [ new_mat * diff_vec for new_mat, diff_vec in zip(diff_mats_q, diff_vec_q) ]
+        # TODO: if calculations are slow, once in a while (i % 100 == 0?) check if gcd(p_i,p_{i-1}) != 0
+        # TODO: and take it out as a factor (add p_gcd, q_gcd params)
         new_vec_p = (sum([l*m for l,m in zip(diff_vec_p, diff_mats_p)]), diff_vec_p[0])
         new_vec_q = (sum([l*m for l,m in zip(diff_vec_q, diff_mats_q)]), diff_vec_q[0])
 
@@ -83,64 +77,46 @@ class PiContFrac(basic_algo.PiBasicAlgo):
         # the first one. Same for new_vec_q
         # p_i = new_vec_p[0][0,0]
         # q_i = new_vec_q[0][0,0]
-        p_i = new_vec_p[0]
-        q_i = new_vec_q[0]
-        if q_i == 0: # q_i.is_zero():
-            pi_i = dec('NaN')
-        else:
-            pi_i = p_i / q_i
-        params['pi'] = pi_i
+        if self._logging:
+            p_i = dec(new_vec_p[0])
+            q_i = dec(new_vec_q[0])
+            if q_i.is_zero():
+                pi_i = dec('NaN')
+            else:
+                pi_i = p_i / q_i
+            params['pi'] = pi_i
         new_diff_mat = (new_vec_p, new_vec_q)
 
+        # OLD COMMENTS - MAY BE IRRELEVANT:
         # the vectors are (p_n, p_{n-1}, dp_n/dx_m, dp_{n-1}/dx_m)
         # so for x_j we take the j'th vector which suits the differentiation by x_j
         # and we take the 3rd element (which is [2]) for the derivative
-        grad = []
-        if not self._with_grad:
-            return (params, new_diff_mat, grad)
+        return (params, new_diff_mat)
 
-        if not pi_i.is_nan():
-            grad.append([ new_vec_p[j][2,0] / q_i - new_vec_q[j][2,0] * p_i/q_i**2 for j in range(len(a_coeffs)) ])
-            grad.append([ new_vec_p[j][2,0] / q_i - new_vec_q[j][2,0] * p_i/q_i**2 for j in
-                          range(len(a_coeffs), len(a_coeffs) + len(b_coeffs)) ])
+    def finalize_iterations(self, params, diff_mat, n):
+        diff_vec_p, diff_vec_q = diff_mat
+        p = dec(diff_vec_p[0])
+        q = dec(diff_vec_q[0])
+        if q.is_zero():
+            pi = dec('NaN')
+        else:
+            pi = p / q
+        params['pi'] = pi
+        return (params, diff_mat)
 
-        return (params, new_diff_mat, grad)
+    def is_accuracy_met(self, accuracy, params, diff_mat, i):
+        # error < gcd^2/q^2 < accuracy
+        # q^2 > gcd^2 * ceil(1 / accuracy) > gcd^2 / accuracy
+        # # diff_vec_p, diff_vec_q = diff_mat
+        # # p = diff_vec_p[0]
+        # # q = diff_vec_q[0]
+        # # pq_gcd = math.gcd(p, q)
+        #
+        # return q**2 > pq_gcd**2 * math.ceil(1/accuracy)
+        if i < 1:
+            return
 
-    def _default_diff_mat_with_grad_gen(self, i, a_coeffs, b_coeffs):
-        a2p = PiContFrac._array_to_polynom
-        a_i = a2p(a_coeffs, i)
-        b_i = a2p(b_coeffs, i)
-        if self._avoid_zero_b and b_i == 0:
-            raise ZeroB()
-        if self._avoid_zero_b and b_i == 0:
-            raise ZeroB()
-        if self._check_b_threshold and b_i / self.gcd(a_i, b_i) > self._b_threshold:
-            raise self.SufficientAccuracy()
-        mat_p = []
-        mat_q = []
-        for j in range(len(a_coeffs)):
-            mat_p.append(np.matrix(( (a_i, b_i, 0, 0),
-                                (1, 0, 0, 0),
-                                (i**j, 0, a_i, b_i),
-                                (0, 0, 1, 0)), self._dtype))
-        for j in range(len(b_coeffs)):
-            mat_p.append(np.matrix(( (a_i, b_i, 0, 0),
-                                (1, 0, 0, 0),
-                                (0, 0, a_i, b_i),
-                                (0, 0, 1, 0)), self._dtype))
-        for j in range(len(a_coeffs)):
-            mat_q.append(np.matrix(((a_i, b_i, 0, 0),
-                                (1, 0, 0, 0),
-                                (0, 0, a_i, b_i),
-                                (0, 0, 1, 0)), self._dtype))
-        for j in range(len(b_coeffs)):
-            mat_q.append(np.matrix(((a_i, b_i, 0, 0),
-                                (1, 0, 0, 0),
-                                (0, i**j, a_i, b_i),
-                                (0, 0, 1, 0)), self._dtype))
-        return (mat_p, mat_q)
-
-    def _default_diff_mat_no_grad_gen(self, i, a_coeffs, b_coeffs):
+    def _default_diff_mat_gen(self, i, a_coeffs, b_coeffs):
         a2p = PiContFrac._array_to_polynom
         a_i = a2p(a_coeffs, i)
         b_i = a2p(b_coeffs, i)
@@ -156,30 +132,7 @@ class PiContFrac(basic_algo.PiBasicAlgo):
         mat_q = mat_p
         return (mat_p, mat_q)
 
-    def _autogen_first_diff_mat_with_grad(self, params, dtype=None):
-        if dtype is None:
-            dtype = self._dtype
-        if hasattr(self, '_dtype') and dtype == self._dtype:
-            dtype_0 = self._dtype_0
-            dtype_1 = self._dtype_1
-        else:
-            dtype_0 = dtype(0)
-            dtype_1 = dtype(1)
-        a_coeffs = params['a_coeffs']
-        b_coeffs = params['b_coeffs']
-        first_diff_mat = ([], [])
-        first_diff_mat[0].extend([np.matrix(((dtype(a_coeffs[0]),), (dtype_1,),
-                                             (dtype(a_coeffs[i] * (i == 0)),), (dtype_0,)), dtype=dtype)
-                                  for i in range(len(a_coeffs))])
-        first_diff_mat[0].extend([np.matrix(((dtype(a_coeffs[0]),), (dtype_1,),
-                                             (dtype_0,), (dtype_0,)), dtype=dtype)
-                                  for i in range(len(a_coeffs))])
-        first_diff_mat[1].extend([np.matrix(((dtype_1,), (dtype_0,),
-                                             (dtype_0,), (dtype_0,)), dtype=dtype)
-                                  for i in range(len(b_coeffs) + len(a_coeffs))])
-        return first_diff_mat
-
-    def _autogen_first_diff_mat_no_grad(self, params, dtype=None):
+    def _autogen_first_diff_mat(self, params, dtype=None):
         if dtype is None:
             dtype = self._dtype
         if hasattr(self, '_dtype') and dtype == self._dtype:
@@ -195,8 +148,6 @@ class PiContFrac(basic_algo.PiBasicAlgo):
         # first_diff_mat[1].append(np.matrix(((dtype_1,), (dtype_0,)), dtype=dtype))
         # return first_diff_mat
         return (dtype(a_coeffs[0]), dtype_1), (dtype_1, dtype_0)
-    # def get_derivative(self):
-    #     return self.compare_result() * self.gradient_vectors[-1]
 
     def compare_result(self, target_val=None):
         if target_val is not None: