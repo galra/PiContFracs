46863bb7efc5ed9d662a3b12426e7067401dfd6a
galra <galraay@gmail.com>
2018-09-02 05:09:52 +0300

    Added a file to run all the distributed configs

---
+++ run_distributed_configs.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def main(configs_dir):
+    files_list = [ os.path.join(configs_dir, f) for f in os.listdir(configs_dir) if os.path.splitext(f)[1] == '.pkl' ]
+    for f in files_list:
+        os.system('start cmd.exe /k main.py %s' % f)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print('%s <configs_dir>' % sys.argv[0])
