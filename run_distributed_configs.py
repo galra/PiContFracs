26fe00f69032264ada6d1f1c219d6f15a84426d2
galra <galraay@gmail.com>
2018-09-02 05:20:43 +0300

    * Minor bug at run distributed configs #3

--- run_distributed_configs.py
+++ run_distributed_configs.py
@@ -5,8 +5,7 @@ import time
 def main(configs_dir):
     files_list = [ os.path.join(configs_dir, f) for f in os.listdir(configs_dir) if os.path.splitext(f)[1] == '.ini' ]
     for f in files_list:
-        # os.system('start cmd.exe /k python main.py %s' % f)
-        os.system('start cmd.exe /k echo hi')
+        os.system('start cmd.exe /k python main.py %s' % f)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
